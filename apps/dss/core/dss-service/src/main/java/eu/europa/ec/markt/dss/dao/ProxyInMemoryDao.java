4026aaf031a3da0933e2e34a40fcceb692aa58e6
bielecro <Robert.Bielecki@arhs-spikeseed.com>
2015-03-13 05:57:28 +0100

    Cosmetic changes

--- apps/dss/core/dss-service/src/main/java/eu/europa/ec/markt/dss/dao/ProxyInMemoryDao.java
+++ apps/dss/core/dss-service/src/main/java/eu/europa/ec/markt/dss/dao/ProxyInMemoryDao.java
@@ -1,3 +1,23 @@
+/*
+ * SD-DSS - Digital Signature Services
+ *
+ * Copyright (C) 2015 ARHS SpikeSeed S.A. (rue Nicolas Bové 2B, L-1253 Luxembourg) http://www.arhs-spikeseed.com
+ *
+ * Developed by: 2015 ARHS SpikeSeed S.A. (rue Nicolas Bové 2B, L-1253 Luxembourg) http://www.arhs-spikeseed.com
+ *
+ * This file is part of the "https://github.com/arhs/sd-dss" project.
+ *
+ * "DSS - Digital Signature Services" is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Lesser General Public License as published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * DSS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * "SD-DSS - Digital Signature Services".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package eu.europa.ec.markt.dss.dao;
 
 import java.util.Collection;
@@ -10,7 +30,6 @@ import org.slf4j.LoggerFactory;
 
 /**
  * An in memory implementation of {@code ProxyDao}
- * <p/>
  *
  * @author Robert Bielecki
  */
